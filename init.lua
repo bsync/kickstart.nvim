14cd5dbe386ba597d098bb2d7e2ab9b3b0719c8f
Travis <james.horine@gmail.com>
2025-12-29 17:51:35 -0600

    Updates to configure opencode ai plugin and autocmds

--- init.lua
+++ init.lua
@@ -135,6 +135,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+  group = vim.api.nvim_create_augroup('kickstart-python-group', { clear = true }),
+  pattern = '*.py',
+  callback = function(event)
+    -- Only set the keymap if it doesn't already exist
+    vim.keymap.set('n', '<leader>db', 'obreakpoint()<Esc>', {
+      desc = 'Set breakpoint',
+      buffer = true, -- Make the mapping local to this buffer
+    })
+    vim.keymap.set('n', '<leader>bp', 'obreakpoint()<Esc>', {
+      desc = 'Set breakpoint',
+      buffer = true, -- Make the mapping local to this buffer
+    })
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -260,7 +276,7 @@ require('lazy').setup({
 
       -- Document existing key chains
       spec = {
-        { '<leader>s', group = '[S]earch' },
+        { '<leader>f', group = '[F]ind' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
         { '<leader>d', group = 'Diagnostics' },
@@ -370,15 +386,17 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
+      vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+      vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[F]ind [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+      vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
+      vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = '[F]ind LSP References' })
+      vim.keymap.set('n', '<leader>fR', builtin.resume, { desc = '[F]ind [R]esume' })
+      vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>ft', ':TermSelect<CR>', { desc = '[F] an open Terminal' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
@@ -400,9 +418,9 @@ require('lazy').setup({
       end, { desc = '[S]earch [/] in Open Files' })
 
       -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
+      vim.keymap.set('n', '<leader>fc', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+      end, { desc = '[F]ind Neovim [C]onfig files' })
     end,
   },
 
@@ -798,7 +816,7 @@ require('lazy').setup({
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'lsp', 'path', 'snippets', 'lazydev', 'buffer' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
         },
--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -1,6 +1,6 @@
 return {
   'NickvanDyke/opencode.nvim',
-  lazy=false,
+  lazy = false,
   dependencies = {
     -- Recommended for `ask()`, and required for `toggle()` — otherwise optional
     { 'folke/snacks.nvim', opts = { input = { enabled = true } } },
@@ -8,9 +8,28 @@ return {
   config = function()
     vim.g.opencode_opts = {
       -- Your configuration, if any — see `lua/opencode/config.lua`
+      provider = { enabled = 'snacks' },
     }
+    vim.keymap.set({ 'n', 'x' }, '<leader>aa', function()
+      require('opencode').ask('@this: ', { submit = true })
+    end, { desc = 'Ask opencode' })
+    vim.keymap.set({ 'n', 'x' }, '<leader>ac', function()
+      require('opencode').select()
+    end, { desc = 'Execute opencode action…' })
+    vim.keymap.set({ 'n', 'x' }, 'ga', function()
+      require('opencode').prompt '@this'
+    end, { desc = 'Add to opencode' })
+    vim.keymap.set({ 'n', 't' }, '<leader>at', function()
+      require('opencode').toggle()
+    end, { desc = 'Toggle opencode' })
+    vim.keymap.set('n', '<S-C-u>', function()
+      require('opencode').command 'session.half.page.up'
+    end, { desc = 'opencode half page up' })
+    vim.keymap.set('n', '<S-C-d>', function()
+      require('opencode').command 'session.half.page.down'
+    end, { desc = 'opencode half page down' })
 
     -- Required for `opts.auto_reload`
     vim.opt.autoread = true
-  end
+  end,
 }
