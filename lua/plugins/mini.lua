1e7fa0fa387c4e28d4c55eefc8c3baf3de737306
James Travis Horine <james.horine@gmail.com>
2026-03-26 20:39:39 -0500

    Add minifiles and codium

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "mini-files/mini.files",
+    "nvim-mini/mini.files",
     event = "VeryLazy",
     keys = {
       { "-", function() require("mini.files").open(vim.api.nvim_buf_get_name(0), true) end, desc = "Open parent directory" },
