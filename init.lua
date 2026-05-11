01e2a745003885bc86deb7ece598762c5e844d78
Travis <james.horine@gmail.com>
2026-05-11 17:34:24 -0500

    Create autocmd to use 'gf' from within terminal normal mode.

--- init.lua
+++ init.lua
@@ -1,2 +1,24 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.keymap.set("n", "gf", function()
+      local cfile = vim.fn.expand("<cfile>")
+      if cfile == "" then return end
+
+      -- Resolve to a real path (walks up from cwd to find it)
+      local path = vim.fn.findfile(cfile, ".;")
+      if path == "" then
+        path = vim.fn.fnamemodify(cfile, ":p")
+      end
+
+      -- Hide the terminal window if there's somewhere else to land
+      if #vim.api.nvim_tabpage_list_wins(0) > 1 then
+        vim.cmd("hide")
+      end
+
+      vim.cmd("edit " .. vim.fn.fnameescape(path))
+    end, { buffer = true, silent = true, desc = "gf: open file, close terminal" })
+  end,
+})
